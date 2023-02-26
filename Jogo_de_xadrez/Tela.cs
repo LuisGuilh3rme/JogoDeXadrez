142fa37204d59d7b9508dbdd72b2d8e0b03fa92e
luis guilherme <luislebert01@outlook.com>
2023-02-26 17:11:00 -0300

    Exibicao de tabuleiro aprimorada

--- Jogo_de_xadrez/Tela.cs
+++ Jogo_de_xadrez/Tela.cs
@@ -9,14 +9,32 @@ namespace Jogo_de_xadrez
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     Peca peca = tab.Peca(i, j);
                     if (peca == null) Console.Write("- ");
-                    else Console.Write("{0} ", peca);
+                    else ExibirPeca(peca);
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void ExibirPeca(Peca peca)
+        {
+
+            ConsoleColor aux = Console.ForegroundColor;
+            if (peca.Cor == Cor.Branca)
+            {                
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue ; 
+            }
+            Console.Write(peca + " ");
+            Console.ForegroundColor = aux;
         }
     }
 }
